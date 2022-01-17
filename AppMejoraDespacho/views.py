08f58d15543ac3c1c2bf632c911e40fd58ad35c5
ElViisho <vicente.videla@ug.uchile.cl>
2022-01-17 12:35:06 -0300

    fixed stupid error

--- AppMejoraDespacho/views.py
+++ AppMejoraDespacho/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db import connections
 
-from AppMejoraDespacho.form import ingresoForm, modifyForm, deleteForm
+from AppMejoraDespacho.form import ingresoForm, deleteForm
 import datetime
 from django.db import connections
 
